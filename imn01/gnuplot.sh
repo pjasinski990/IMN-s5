a4466a6103c30ffcb2650cbd8d9be735812f0850
Piotr Jasinski <piotr.jasinski990@gmail.com>
2020-10-20 22:03:00 +0200

    done?

--- imn01/gnuplot.sh
+++ imn01/gnuplot.sh
@@ -1,12 +1,56 @@
 set style line 1 \
-    linecolor rgb '#0060ad' \
-    linetype 1 linewidth 2 \
-    pointtype 7 pointsize 1.5
+    linecolor rgb "red" \
+    linetype 1 linewidth 2
 set style line 2 \
-    linecolor rgb '#dd181f' \
-    linetype 1 linewidth 2 \
-    pointtype 5 pointsize 1.5
+    linecolor rgb "green" \
+    linetype 1 linewidth 2
+set style line 3 \
+    linecolor rgb "royalblue" \
+    linetype 1 linewidth 2
+set style line 3 \
+    linecolor rgb "violet" \
+    linetype 1 linewidth 2
 
-set output "a.eps"
-plot 'a.dat' index 0 with linespoints linestyle 1, \
-    ''                   index 1 with linespoints linestyle 2
+set term png
+
+set output "1a.png"
+plot '1a.dat' using 1:2 index 0 with linespoints linestyle 1 t "dt = 0.01", \
+    ''      using 1:2 index 1 with linespoints linestyle 2 t "dt = 0.1", \
+    ''      using 1:2 index 2 with linespoints linestyle 3 t "dt = 1" 
+
+set output "1a_err.png"
+plot '1a.dat' using 1:3 index 0 with linespoints linestyle 1 t "dt = 0.01", \
+    ''      using 1:3 index 1 with linespoints linestyle 2 t "dt = 0.1", \
+    ''      using 1:3 index 2 with linespoints linestyle 3 t "dt = 1" 
+
+set output "1b.png"
+plot '1b.dat' using 1:2 index 0 with linespoints linestyle 1 t "dt = 0.01", \
+    ''      using 1:2 index 1 with linespoints linestyle 2 t "dt = 0.1", \
+    ''      using 1:2 index 2 with linespoints linestyle 3 t "dt = 1" 
+
+set output "1b_err.png"
+plot '1b.dat' using 1:3 index 0 with linespoints linestyle 1 t "dt = 0.01", \
+    ''      using 1:3 index 1 with linespoints linestyle 2 t "dt = 0.1", \
+    ''      using 1:3 index 2 with linespoints linestyle 3 t "dt = 1" 
+
+set output "1c.png"
+plot '1c.dat' using 1:2 index 0 with linespoints linestyle 1 t "dt = 0.01", \
+    ''      using 1:2 index 1 with linespoints linestyle 2 t "dt = 0.1", \
+    ''      using 1:2 index 2 with linespoints linestyle 3 t "dt = 1" 
+
+set output "1c_err.png"
+plot '1c.dat' using 1:3 index 0 with linespoints linestyle 1 t "dt = 0.01", \
+    ''      using 1:3 index 1 with linespoints linestyle 2 t "dt = 0.1", \
+    ''      using 1:3 index 2 with linespoints linestyle 3 t "dt = 1" 
+
+set output "2_Q.png"
+plot '2.dat' using 1:2 index 0 with linespoints linestyle 1 t "Q = f(t), 0.5*omega", \
+    ''      using 1:2 index 1 with linespoints linestyle 2 t "Q = f(t), 0.8*omega", \
+    ''      using 1:2 index 2 with linespoints linestyle 3 t "Q = f(t), 1.0*omega", \
+    ''      using 1:2 index 3 with linespoints linestyle 4 t "Q = f(t), 1.2*omega" 
+
+set output "2_I.png"
+plot '2.dat' using 1:3 index 0 with linespoints linestyle 1 t "I = f(t), 0.5*omega", \
+    ''      using 1:3 index 1 with linespoints linestyle 2 t "I = f(t), 0.8*omega", \
+    ''      using 1:3 index 2 with linespoints linestyle 3 t "I = f(t), 1.0*omega", \
+    ''      using 1:3 index 3 with linespoints linestyle 4 t "I = f(t), 1.2*omega" 
--- imn01/main.c
+++ imn01/main.c
@@ -1,8 +1,18 @@
+#define _USE_MATH_DEFINES
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+
+double exact(double lambda, double t)
+{
+    return exp(t * lambda);
+}
 
 int main(int argc, char const *argv[])
 {
+    // Zadanie 1sze 
+
     const double y0 = 0.0;
     const double fy0 = 1.0;
     const double lambda = -1.0;
@@ -10,14 +20,15 @@ int main(int argc, char const *argv[])
     const double tmax = 5.0;
     const double ts[3] = {0.01, 0.1, 1.0};
 
-    FILE* f1 = fopen("a.dat", "w");
+    // Euler
+    FILE* f1 = fopen("1a.dat", "w");
     for (int i = 0; i < 3; i++)
     {
         double t = tmin;
         double y = fy0;
         while (t <= tmax)
         {
-            fprintf(f1, "%f %f\n", t, y);
+            fprintf(f1, "%f %f %f\n", t, y, fabs(y - exact(lambda, t)));
             t += ts[i];
             y += y*lambda*ts[i];
         }
@@ -25,5 +36,93 @@ int main(int argc, char const *argv[])
     }
     fclose(f1);
 
+    // jawna RK2 (trapezow)
+    FILE* f2 = fopen("1b.dat", "w");
+    for (int i = 0; i < 3; i++)
+    {
+        double t = tmin;
+        double y = fy0;
+        double k1, k2;
+        while (t <= tmax)
+        {
+            k1 = lambda * y;
+            k2 = lambda * (y + ts[i] * k1);
+            fprintf(f2, "%f %f %f\n", t, y, fabs(y - exact(lambda, t)));
+            t += ts[i];
+            y += ts[i]/2 * (k1 + k2);
+        }
+        fprintf(f2, "\n\n");
+    }
+    fclose(f2);
+
+    // jawna RK4
+    FILE* f3 = fopen("1c.dat", "w");
+    for (int i = 0; i < 3; i++)
+    {
+        double t = tmin;
+        double y = fy0;
+        double k1, k2, k3, k4;
+        while (t <= tmax)
+        {
+            k1 = lambda * y;
+            k2 = lambda * (y + ts[i]/2 * k1);
+            k3 = lambda * (y + ts[i]/2 * k2);
+            k4 = lambda * (y + ts[i] * k3);
+            fprintf(f3, "%f %f %f\n", t, y, fabs(y - exact(lambda, t)));
+            t += ts[i];
+            y += ts[i]/6 * (k1 + 2*k2 + 2*k3 + k4);
+        }
+        fprintf(f3, "\n\n");
+    }
+    fclose(f3);
+
+
+    // Zadanie 2 - RRZ 2go rzedu
+
+    double V(double omegaV, double t) 
+    {
+        return 10 * sin(omegaV * t);
+    }
+
+    const double dt = 10e-4;
+    const double R = 100;
+    const double L = 0.1;
+    const double C = 0.001;
+    const double omega0 = 1 / sqrt(L*C);
+    const double T0 = 2 * M_PI / omega0;
+    const double tmin2 = 0.0;
+    const double tmax2 = 4.0 * T0;
+    double k1q, k2q, k3q, k4q, k1i, k2i, k3i, k4i;
+
+    const double Q0 = 0;
+    const double I0 = 0;
+
+    double omegas[] = {0.5*omega0, 0.8*omega0, omega0, 1.2*omega0};
+
+    FILE* f4 = fopen("2.dat", "w");
+    for (int i = 0; i < 4; i++)
+    {
+        double t = tmin2;
+        double Q = Q0;
+        double I = I0;
+        while (t <= tmax2) 
+        {
+            fprintf(f4, "%f %f %f\n", t, Q, I);
+            k1q = I;
+            k1i = V(omegas[i], t);
+            k2q = I + dt/2 * k1i;
+            k2i = V(omegas[i], t + dt/2) / L + 1/(L*C) * (Q + dt/2 * k1q) - R/L * (I + dt/2 * k1i);
+            k3q = I + dt/2 * k2i;
+            k3i = V(omegas[i], t + dt/2) / L - 1/(L*C) * (Q + dt/2 * k2q) - R/L * (I + dt/2 * k2i);
+            k4q = I + dt * k3i;
+            k4i = V(omegas[i], t + dt) / L - 1/(L*C) * (Q + dt * k3q) - R/L * (I + dt * k3i);
+
+            Q += dt/6 * (k1q + 2*k2q + 2*k3q + k4q);
+            I += dt/6 * (k1i + 2*k2i + 2*k3q + k4i);
+            t += dt;
+        }
+        fprintf(f4, "\n\n");
+    }
+    fclose(f4);
     return 0;
 }
